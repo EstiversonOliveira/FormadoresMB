e39d9169f55830f5d72e574e6c756bce5ff89997
Estíverson Oliveira <107802352+EstiversonOliveira@users.noreply.github.com>
2024-07-04 20:16:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,13 @@ const perguntas = [
         ]
     } 
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntas[atual].enunciado;
+}
+
+mostraPergunta();
